23a42770a83bca1b039093a86dc9147789a3509d
Austin <awood076@gmail.com>
2023-11-09 15:15:25 -0500

    Erase test comment

--- app/src/main/java/com/example/windbreaker/MainActivity.kt
+++ app/src/main/java/com/example/windbreaker/MainActivity.kt
@@ -3,8 +3,6 @@ package com.example.windbreaker
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-// Test 4
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
